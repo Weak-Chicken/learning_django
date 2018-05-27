8657609d9111541efe6d57897adbafe256f7fc84
Jon <aoppy199999@gmail.com>
2018-05-27 16:46:37 +0100

    Delete & Recreate

--- djangoproject/posts/views.py
+++ djangoproject/posts/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Posts
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello from Posts')
+    # return HttpResponse('Hello from Posts')
+
+    posts = Posts.objects.all()[:10]
+
+    context = { 
+        'title': 'Latest Posts',
+        'posts': posts
+    }
+
+    return render(request, 'posts/index.html', context)
